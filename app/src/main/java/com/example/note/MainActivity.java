f50d8032c3f7021af9aeddafbd916b0485209268
zepingwang <1512796976@qq.com>
2020-03-28 05:53:08 -0500

    11

--- app/src/main/java/com/example/note/MainActivity.java
+++ app/src/main/java/com/example/note/MainActivity.java
@@ -2,61 +2,104 @@ package com.example.note;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.TextView;
+import android.widget.Toast;
+import android.database.Cursor;
 
 public class MainActivity extends AppCompatActivity {
 
-    int listNum = 0;
+    NoteModel DBMD;
+    TextView balance;
+    EditText Date, Item, Price;
+    Button Add, Sub;
+    TextView history;
 
-    EditText txtID,txtContent, txtTitle, txtTags;
-    Button btnLoad, btnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        DBMD = new NoteModel(this);
+        balance = (TextView) findViewById(R.id.balance);
+        Date = (EditText) findViewById(R.id.Date);
+        Price = (EditText) findViewById(R.id.Price);
+        Item = (EditText) findViewById(R.id.Item);
+        Add = (Button) findViewById(R.id.Add);
+        Sub = (Button) findViewById(R.id.Sub);
+        history = (TextView) findViewById(R.id.history);
+        addHistory();
+        retHistory();
+    }
 
-        txtTitle = findViewById(R.id.txtTitle);
-        txtContent = findViewById(R.id.txtContent);
+    public void addHistory(){
+        Add.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        double price = Double.parseDouble(Price.getText().toString());
+                        boolean result = DBMD.createHistory(Item.getText().toString(), Date.getText().toString(), price);
+                        if (result)
+                            Toast.makeText(MainActivity.this, "Successfully Created", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Failed to Create", Toast.LENGTH_LONG).show();
+                        retHistory();
+                        MainActivity.this.Date.setText("");
+                        MainActivity.this.Price.setText("");
+
+                        MainActivity.this.Item.setText("");
+                    }
+                }
+        );
+
+        Sub.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        double price = -1 * Double.parseDouble(Price.getText().toString());
+                        boolean result = DBMD.createHistory(Item.getText().toString(), Date.getText().toString(), price);
+                        if (result)
+                            Toast.makeText(MainActivity.this, "Successfully Created", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Failed to Create", Toast.LENGTH_LONG).show();
+                        retHistory();
+                        MainActivity.this.Date.setText("");
+                        MainActivity.this.Price.setText("");
+
+                        MainActivity.this.Item.setText("");
+                    }
+                }
+        );
+    }
+
+    public void retHistory(){
+        Cursor result = DBMD.pullData();
+        StringBuffer str = new StringBuffer();
+        Double balance = 0.0;
+
+        while(result.moveToNext()){
+            String priceString = result.getString(3);
+            double price = Double.parseDouble(result.getString(3));
+            balance += price;
+
+            if (price < 0) {
+                str.append("Spent $");
+                priceString = priceString.substring(1);
 
 
-        btnSave = findViewById(R.id.btnSave);
-
-
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                listNum++;
-                NoteModel note = new NoteModel();
-                note.title = txtTitle.getText().toString();
-                note.content = note.title;
-
-                SharedPreferences pref = getPreferences(0);
-                SharedPreferences.Editor editor = pref.edit();
-
-                String json = note.toJson();
-                Log.i("json", json);
-
-                note.fromJson(json);
-
-                editor.putString(note.title, json);
-                editor.commit();
-
-                txtTitle.setText(note.title);
-                txtContent.setText(txtContent.getText().toString()+"\n"+ listNum + ". " +note.content);
 
 
             }
-        });
-
-
+            else {
+                str.append("Added $");
+            }
+            str.append(priceString + " on " + result.getString(2)
+                    + " for " + result.getString(1) + "\n");
+        }
+        MainActivity.this.balance.setText("Current Balance: $" + Double.toString(balance));
+        MainActivity.this.history.setText(str);
     }
 }
--- app/src/main/java/com/example/note/NoteModel.java
+++ app/src/main/java/com/example/note/NoteModel.java
@@ -1,16 +1,50 @@
 package com.example.note;
 
-import com.google.gson.Gson;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+import android.content.ContentValues;
 
-public class NoteModel {
-    public String  title, content;
+public class NoteModel extends SQLiteOpenHelper {
 
-    public String toJson() {
-        Gson gson = new Gson();
-        return gson.toJson(this);
+    public static final String DBName = "spending.db";
+    public static final String TableName = "SpendingTable";
+    public static final String ItemName = "Item";
+    public static final String DateName = "Date";
+    public static final String PriceName = "Price";
+
+    public NoteModel(Context context){
+        super(context, DBName, null, 4);
     }
-    public static NoteModel fromJson(String json) {
-        Gson gson = new Gson();
-        return gson.fromJson(json, NoteModel.class);
+
+    public void onCreate(SQLiteDatabase sqlDB){
+        sqlDB.execSQL("CREATE TABLE " + TableName + " (Count INTEGER PRIMARY KEY AUTOINCREMENT, Item varchar(100), Date varchar(100), Price DOUBLE)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqlDB, int older, int newer){
+        sqlDB.execSQL("DROP TABLE IF EXISTS " + TableName);
+        onCreate(sqlDB);
+    }
+
+
+    public boolean createHistory(String item, String data, double price){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ItemName, item);
+        contentValues.put(DateName, data);
+        contentValues.put(PriceName, price);
+        long result = db.insert(TableName, null, contentValues);
+        if (result == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public Cursor pullData() {
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
+        Cursor res = sqlDB.rawQuery("SELECT * FROM " + TableName, null);
+        return res;
     }
 }
